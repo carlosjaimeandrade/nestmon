d2de2a47fa7c048771d19af75be8ba9d9534dcb2
Carlos Jaime <jaime_andrek@hotmail.com>
2023-03-16 10:12:27 -0300

    criando schema de usuários

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from './schemas/users.schema';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'User',
+        schema: UserSchema
+      }
+    ]),
+    AuthModule
+  ],
   controllers: [UsersController],
   providers: [UsersService]
 })
