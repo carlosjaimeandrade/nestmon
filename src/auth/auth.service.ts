4191ee50033affb947b8e7f06168ce6a145d5a2c
Carlos Jaime <jaime_andrek@hotmail.com>
2023-03-21 09:36:28 -0300

    criando jwt strategy

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,6 +4,7 @@ import { Model } from 'mongoose';
 import { User } from 'src/users/models/users.modal';
 import { sign } from 'jsonwebtoken';
 import { Request } from 'express';
+import { JwtPayload } from './models/jwt-payload.model';
 
 @Injectable()
 export class AuthService {
@@ -18,8 +19,8 @@ export class AuthService {
         })
     }
 
-    public async validateUser(userId: string): Promise<User> {
-        const user = await this.usersModel.findOne({ _id: userId })
+    public async validateUser(jwtPayload: JwtPayload): Promise<User> {
+        const user = await this.usersModel.findOne({ _id: jwtPayload.userId })
         if(!user) {
             throw new UnauthorizedException('User not found')
         }
